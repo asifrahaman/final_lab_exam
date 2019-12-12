70d181b8f62193bd8225dc6790f19d5ab717c937
Asif Rahaman <55274445+asifrahaman@users.noreply.github.com>
2019-12-12 10:16:48 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up • MINI</title>
+</head>
+	<body data-gr-c-s-loaded="true">
+		
+
+	<table align="center" width="100%" border="0" >
+		<tbody align="center"><tr>
+			<td>
+				<br>
+				<br>
+					<form method="POST">
+						
+						<label for="id"><b>ID</b></label><br>
+						<input type="text" name="name" id="name" placeholder="Enter A ID" value="" size="35"> <br><br>	
+						<label for="password"><b>Password</b></label><br>
+						<input type="password" name="password" id="password" placeholder="Password"size="35"> <br><br>
+						<label for="repassword"><b>Re-Type Password</b></label><br>
+						<input type="password" name="repassword" id="repassword" placeholder="Re-Type Password"size="35"> <br><br>
+						<label for="name"><b>Name</b></label><br>
+						<input type="text" name="name" id="name" placeholder="Full name" value="" size="35"> <br><br>
+						<label for="email"><b>Email</b></label><br>
+						<input type="text" name="email" id="email" placeholder="Email" value="" size="35"> <br><br>
+						<label for="usertype"><b>User Type</b></label><br>
+						<select name="user_type" id="user_type">
+							<option value="">Admin</option>
+							<option value="">User</option><br><br>
+						<input type="submit" name="submit" value="Sign Up">
+					</form>
+					<br>
+					<p align="center">Have an account? <a href="index.php"><font><b>Log In Here</b></font></a></p>
+					<br>
+					<br>
+					
+			</td>
+		</tr>
+	</tbody></table>
+
+
+</body>
+</html>
